680b3a21fcd72588bfd2cfc4c67d640a5b3aed9c
SantiagoGBS <99350629+SantiagoGBS@users.noreply.github.com>
2023-07-14 19:40:15 +0200

    -Añado framerate y frameBuffer al constructor. -Creo condicional donde compruebo el tipo de imagen. -Añado el bucle para crear el movimiento mediante updateFrames

--- js/classes/Sprite.js
+++ js/classes/Sprite.js
@@ -1,40 +1,65 @@
 class Sprite {
-    constructor({position, imageSrc}){
+    constructor({position, imageSrc, frameRate=1, frameBuffer =47, type = null}){
        this.position = position
        this.image = new Image()
         this.image.onload=()=>{
            this.width = this.image.width
             this.height = this.image.height
         }
-       this.image.src = imageSrc
+        this.image.src = imageSrc
+        this.frameRate = frameRate
+        this.currentFrame=0
+        this.frameBuffer=frameBuffer
+        this.elapsedFrames=0
+        this.type = type
     }
 
     draw(){
        if(!this.image) return
+       // c.drawImage(this.image, this.position.x, this.position.y)
+        console.log(this.type)
+        if (this.type != null) {
+            const frame ={
+                position:{
+                    x:this.currentFrame * (this.image.width /this.frameRate),
+                    y:0,
+                },
+                width:50,
+                height:50,
+            }
+                c.drawImage(
+                    this.image,
+                    frame.position.x,
+                    frame.position.y,
+                    frame.width,
+                    frame.height,
+                    this.position.x,
+                    this.position.y,
+                    this.width,
+                    this.height
+                )
 
-        const frame ={
-           position:{
-               x:0,
-               y:0
-           },
-            width:0,
-            height:0,
+        } else {
+            c.drawImage(
+                this.image,
+                this.position.x,
+                this.position.y,
+                this.width,
+                this.height
+            )
         }
-
-       c.drawImage(
-           this.image,
-           frame.position.x,
-           frame.position.y,
-           frame.width,
-           frame.height,
-           this.position.x,
-           this.position.y,
-           this.width,
-           this.height
-           )
     }
 
     update(){
        this.draw()
+        this.updateFrames()
+    }
+    updateFrames(){
+        this.elapsedFrames++
+        if(this.elapsedFrames % this.frameBuffer ===0){
+            if(this.currentFrame <this.frameRate - 1) this.currentFrame++
+            else this.currentFrame =0
+        }
+
     }
 }
